e11bbec84c0d57cc347c2f3e7199bfbb897affa5
xw.long <xuewu1011@163.com>
2016-12-29 23:15:12 +0800

    for pull

--- FMYWeath/FMYWeath/CustomViews/FMYWAlmanacPlatView.swift
+++ FMYWeath/FMYWeath/CustomViews/FMYWAlmanacPlatView.swift
@@ -1,4 +1,3 @@
-lseek(<#T##Int32#>, <#T##off_t#>, <#T##Int32#>)//
 //  FMYWAlmanacPlatView.swift
 //  FMYWeath
 //
@@ -10,12 +9,11 @@ import UIKit
 
 class FMYWAlmanacPlatView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    var labelDate:UILabel?
+    var labelWeek:UILabel?
+
+    var labelWeek:UILabel?
+    var labelWeek:UILabel?
+
 
 }
